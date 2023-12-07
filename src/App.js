66c8aaba881dcfdca21bae452ba22498d0ed5e40
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-12-07 16:23:26 +0100

    Updated function App in App.js to display No. of people and names + craft - buttons with no function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const URL = `http://api.open-notify.org/astros.json`;
 
 function App() {
+  const [peopleInSpace, setPeopleInSpace] = useState(0);
+  const [namePeople, setNamePeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      const response = await fetch(URL);
+      const data = await response.json();
+
+      setPeopleInSpace(data.number);
+      setNamePeople(data.people);
+    }
+    fetchPeople();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>In the space we currently can see: {peopleInSpace} people.</p>
+        <ul>
+          {namePeople.map(({ name, craft }) => (
+            <li key={name}>
+              {name} - Ship: {craft}
+            </li>
+          ))}
+        </ul>
+        <button type="button">All</button>
+        <button type="button">ISS</button>
+        <button type="button">Tiangong</button>
       </header>
     </div>
   );
